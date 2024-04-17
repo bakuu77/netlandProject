bf07eaae87a8e54e82b234960ba111ac1c0de61a
Blazej <blazejsz5@wp.pl>
2024-04-17 19:40:09 +0200

    Export enum, export interface

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,40 +2,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from '../app.component';
-
-enum Positions {
-  Junior = "Junior",
-  Mid = "Mid",
-  Senior = "Senior",
-  Manager = "Manager"
-}
+import { Position } from '../positions.enum';
 
 @Component({
   selector: 'app-form',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
-  template: `
-    <p>
-      form works!
-    </p>
-    <form [formGroup]="applyForm" (submit)="submitApplication()">
-      <label for="imieNazwisko">Imie i nazwisko:</label>
-      <input type="text" id="imieNazwisko" formControlName="name">
-      <br>
-      <label for="wiek">Wiek:</label>
-      <input type="number" id="wiek" formControlName="age">
-      <br>
-      <label for="pelnyEtat">Czy pracuje na pelny etat?</label>
-      <input type="checkbox" id="pelnyEtat" formControlName="isFullTime">
-      <br>
-      <label for="Pozycja">Na jakiej pozycji sie znajduje?</label>
-      <select id="Pozycja" formControlName="position">
-        <option *ngFor="let pozycja of pozycje | keyvalue: sortPos">{{pozycja.value}}</option>
-      </select>
-      <hr>
-      <button type="submit" class="primary">Zastosuj</button>
-  `,
+  templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
@@ -48,6 +21,6 @@ export class FormComponent {
   submitApplication() {
     console.log(this.applyForm.value)
   };
-  pozycje = Positions;
+  pozycje = Position;
   sortPos() { return 0 };
 }
