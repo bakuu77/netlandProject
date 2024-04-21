814485a1e5dbbeb3cb6f1f37959134701ceb963c
Blazej <blazejsz5@wp.pl>
2024-04-21 20:56:30 +0200

    Auto-generated commit

--- src/app/employee.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Position } from './positions.enum';
-
-export interface EmployeeNetland {
-    id: number,
-    name: string;
-    age: number;
-    isFullTime: boolean;
-    position: Position;
-}
